ebd52dfff5269791eeff821ff542d025a5b81fdf
Matt Osborne <MattJOzzy@gmail.com>
2023-05-31 18:06:22 -0500

    updated styling and content on Footer, Hero, and Navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="text-white">
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
         <p className="text-[#00df9a] font-bold p-2">
-          GROWING WITH DATA ANALYTICS
+          DATA ANALYTICS
         </p>
         <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
           Grow with data.
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
         <p className="md:text-2xl text-xl font-bold text-gray-500">
           Monitor your data analytics to increase revenue for BTB, BTC, & SASS
-          platforms.
+          platforms and more.
         </p>
         <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black hover:bg-white">
           Get Started
