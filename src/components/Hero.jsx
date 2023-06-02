bb03c3ce05b1f3fe26d556b628411c74ae0d5879
Matt Osborne <MattJOzzy@gmail.com>
2023-06-01 22:34:12 -0500

    updated content in hero and nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,26 +6,25 @@ const Hero = () => {
     <div className="text-white">
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
         <p className="text-[#00df9a] font-bold p-2">
-          DATA ANALYTICS
+          Hi, I'm a
         </p>
         <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
-          Grow with data.
+          Front End Developer 
         </h1>
         <div className="flex justify-center items-center">
           <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
-            Fast, flexible financing for
+            Creating websites using:
           </p>
           <Typed
             className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
-            strings={["BTB", "BTC", "SASS"]}
+            strings={["React JS", "Angular", "Vue"]}
             typeSpeed={120}
             backSpeed={140}
             loop
           />
         </div>
         <p className="md:text-2xl text-xl font-bold text-gray-500">
-          Monitor your data analytics to increase revenue for BTB, BTC, & SASS
-          platforms and more.
+        Welcome to my portfolio, where creativity and functionality converge to deliver exceptional user experiences through innovative front-end development.
         </p>
         <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black hover:bg-white">
           Get Started
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,20 +10,20 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">GREEN DATA</h1>
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">MATT OSBORNE</h1>
       <ul className="hidden md:flex">
         <ul className="hidden md:flex">
           <li className="p-4 hover:text-[#00df9a]">
             <a href="#">Home</a>
           </li>
           <li className="p-4 hover:text-[#00df9a]">
-            <a href="#">Company</a>
-          </li>
-          <li className="p-4 hover:text-[#00df9a]">
-            <a href="#">Resources</a>
-          </li>
-          <li className="p-4 hover:text-[#00df9a]">
             <a href="#">About</a>
+          </li>
+          <li className="p-4 hover:text-[#00df9a]">
+            <a href="#">Portfolio</a>
+          </li>
+          <li className="p-4 hover:text-[#00df9a]">
+            <a href="#">Experience</a>
           </li>
           <li className="p-4 hover:text-[#00df9a]">
             <a href="#">Contact</a>
@@ -40,11 +40,12 @@ const Navbar = () => {
             : "ease-in-out duration-500 fixed left-[-100%]"
         }
       >
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">MATT OSBORNE</h1>
         <li className="p-4 border-b border-gray-600">Home</li>
-        <li className="p-4 border-b border-gray-600">Company</li>
-        <li className="p-4 border-b border-gray-600">Resources</li>
         <li className="p-4 border-b border-gray-600">About</li>
+        <li className="p-4 border-b border-gray-600">Portfolio</li>
+        <li className="p-4 border-b border-gray-600">Experience</li>
+        <li className="p-4 border-b border-gray-600">Contact</li>
         <li className="p-4">Contact</li>
       </ul>
     </div>
