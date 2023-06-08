8d5b7f0c649d1a21d72073ef3a93edb9c935c0de
Matt Osborne <MattJOzzy@gmail.com>
2023-06-07 23:29:09 -0500

    updated content in newsletter section

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -8,7 +8,7 @@ const Newsletter = () => {
           <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>
             Want tips & tricks to optimize your flow?
           </h1>
-          <p>Sign up to our newsletter and stay up to date.</p>
+          <p>Sign up to my newsletter and stay up to date.</p>
         </div>
         <div className='my-4'>
           <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
